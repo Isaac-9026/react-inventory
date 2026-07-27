1af0523e551ddb4044552852f199011c8eded066
Soy Isaac <isaipaucarchuquispuma@gmail.com>
2026-07-26 21:55:18 -0500

    style: agregar estilos base a componentes del catálogo

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,15 @@
 import ProductRow from '../ProductRow/ProductRow'
+import './ProductList.css'
 
 
 function ProductList({ products }){
 
     return(
-        products.map(product => (
+        <div className="product-list">
+        {products.map(product => (
             <ProductRow key= {product.id} product = {product}/>
-        ))
+        ))}
+        </div>
     )
 }
 
--- src/components/ProductRow/ProductRow.jsx
+++ src/components/ProductRow/ProductRow.jsx
@@ -1,13 +1,15 @@
+import './ProductRow.css'
+
 function ProductRow ({ product }) {
 
     return(
-        <>
+        <article className="product-row">
             <h3>{product.nombre}</h3>
             <p>Categoria: {product.categoria}</p>
             <p>Marca: {product.marca}</p>
             <p>Precio: {product.precio}</p>
             <p>Stock: {product.stock}</p>
-        </>
+        </article>
             
     )
 }
