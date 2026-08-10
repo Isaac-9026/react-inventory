5a1d23d6846334da8d1779224a887a40e087050f
Soy Isaac <isaipaucarchuquispuma@gmail.com>
2026-08-09 19:44:05 -0500

    feat: agregar tipo Product.

---
+++ src/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  nombre: string;
+  marca: string;
+  categoria: string;
+  modelo: string;
+  descripcion: string;
+  precio: number;
+  stock: number;
+}
